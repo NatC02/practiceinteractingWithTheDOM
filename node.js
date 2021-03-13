ce4e4283abbfc26354db6b13e8b5985a36c72876
NatC02 <NatanCeballos02@gmail.com>
2021-03-13 07:40:43 -0500

    removing files to add for review in pull request

--- node.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-    const requestListener = function (req, res) {
-    res.writeHead(200);
-    res.end('Sorry the rest of the content is locked behind dlc');
-        }
-
-const server = http.createServer(requestListener);
-server.listen(8080);
